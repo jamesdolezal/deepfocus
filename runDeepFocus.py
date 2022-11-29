bd0fb9773c40b52eb8f798e19b298c9772a7d37e
James <jamesmdolezal@gmail.com>
2022-11-28 20:09:48 -0600

    Style cleanup

--- runDeepFocus.py
+++ runDeepFocus.py
@@ -1,106 +1,113 @@
-from __future__ import division, print_function, absolute_import
-import openslide
+import click
 import numpy
 import matplotlib.pyplot as plt
-from skimage import filters
-from skimage import color
+import openslide
 import tflearn
-import sys
 import numpy as np
 import classificationModel3
 import hyperparameterModel
-import platform
-import time
 import os
-import glob
 import csv
-#version 1.0 generated on 6/15/2016
+from os.path import basename, join, exists
+from skimage import filters, color
+from typing import Callable, Tuple
+from tqdm import tqdm
 
 
-def writeXML(filename,data):
-    with open(filename, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def analyze(
+    slide_path: str,
+    model: Callable,
+    out: str = 'output',
+    kernel_size: int = 64,
+    buffer: int = 8,
+    step_size: int = 1
+) -> Tuple[int, int]:
+    """Analyze a given image using DeepFocus.
 
-def analyze(imgpath,model):
+    Args:
+        buffer (int): Will load kernel_size x buffer."""
 
-    imgname=os.path.basename(imgpath)
-    kernelSize=64;
-    kernelStepSize=1;
-    bufferVal = 8# will load kernelSize x bufferVal
-    stepsize=1
-    starttime=time.time()
+    # Initial preparation
+    counter1 = 0
+    counter2 = 0
+    outputsVec = []
+    buffersize = kernel_size * buffer
+    k = kernel_size / step_size
+    if not exists(out):
+        os.makedirs(out)
 
-    slide = openslide.open_slide(imgpath)
-    #tissue detection
-    thumbnail=np.array (slide.get_thumbnail((slide.level_dimensions[0][0]/64,slide.level_dimensions[0][1]/64)))
-    thumbnailGray = color.rgb2gray(thumbnail)
-    val = filters.threshold_otsu(thumbnailGray)
-    tissueMask = thumbnailGray < max(val,0.8)
-    plt.imsave('tissue.png', tissueMask) #save the thumb of tissue mask
+    # Load slide & detect tissue via Otsu thresholding
+    slide = openslide.open_slide(slide_path)
+    thumbnail = np.array(slide.get_thumbnail((slide.level_dimensions[0][0]/kernel_size,
+                                              slide.level_dimensions[0][1]/kernel_size)))
+    gray_thumbnail = color.rgb2gray(thumbnail)
+    val = filters.threshold_otsu(gray_thumbnail)
+    tissue_mask = gray_thumbnail < max(val,0.8)
+    plt.imsave(join(out, 'tissue.png'), tissue_mask)
+    merged_mask = np.zeros_like(thumbnail, dtype=np.uint8)
+    if step_size > 1:
+        new_shape = (int(merged_mask.shape[0] / step_size),
+                     int(merged_mask.shape[1] / step_size),
+                     merged_mask.shape[2])
+        merged_mask = np.resize(merged_mask, new_shape)
 
-    buffersize=kernelSize*bufferVal;
-    resultMask = thumbnail.astype(numpy.uint8) * 0;
-    if stepsize>1:
-        resultMask=np.resize(resultMask,( int(resultMask.shape[0]/stepsize) , int(resultMask.shape[1]/stepsize) ,resultMask.shape[2]))
-    counter1=0
-    counter2=0
-    expectedStep = tissueMask.shape[0] / bufferVal
-    outputsVec=[]
-    for i in range( 0,tissueMask.shape[0]-bufferVal,bufferVal): #Height
+    for i in tqdm(range(0,tissue_mask.shape[0] - buffer, buffer)): # Height
+        for j in range(0,tissue_mask.shape[1] - buffer, buffer): # Width
 
-        curMod= i %  (bufferVal*max(5,int(expectedStep/20)))
-        if curMod==0:
-            print('.', end='', flush=True)
-        for j in range(0,tissueMask.shape[1]-bufferVal,bufferVal): #Width
-
-            if np.mean(tissueMask[i:i+bufferVal,j:j+bufferVal])< (8/16):  #most of them are background
+            # Check for background; if so, skip
+            if np.mean(tissue_mask[i:i + buffer, j:j + buffer])< (8/16):
                 continue
-            bigTile=numpy.array(slide.read_region((j*kernelSize,i*kernelSize),0,[buffersize,buffersize]))
-            bigTile=color.rgba2rgb(bigTile)
+
+            bigTile = numpy.array(slide.read_region((j * kernel_size, i * kernel_size), 0, [buffersize, buffersize]))
+            bigTile = color.rgba2rgb(bigTile)
             sz = bigTile.itemsize
-            h, w,cs = bigTile.shape
-            bh, bw =kernelSize,kernelSize
-            shape = (int(h / bh/stepsize), int(w / bw/stepsize), bh, bw, cs)
-            strides = (stepsize*w * bh * sz * cs, stepsize * sz * cs*bw, w * sz * cs, sz * cs, sz)
-            blocks = np.lib.stride_tricks.as_strided(bigTile, shape=shape, strides=strides)
-            blocks= blocks.reshape(blocks.shape[0]*blocks.shape[1], blocks.shape[2], blocks.shape[3], blocks.shape[4])
+            h, w, c = bigTile.shape
+            strided_shape = (int(h / k), int(w / k), kernel_size, kernel_size, c)
+            strides = (step_size * kernel_size * sz * c * w,
+                       step_size * kernel_size * sz * c,
+                       sz * c * w,
+                       sz * c,
+                       sz)
+            blocks = np.lib.stride_tricks.as_strided(bigTile, shape=strided_shape, strides=strides)
+            blocks = blocks.reshape(blocks.shape[0] * blocks.shape[1],
+                                    blocks.shape[2],
+                                    blocks.shape[3],
+                                    blocks.shape[4])
             predictions = model.predict(blocks)
-            outputsVec=outputsVec+predictions
+            outputsVec.append(predictions)
             qwe = np.array(predictions)
-            qwe = qwe.reshape(int(h / bh/stepsize), int(w / bw / stepsize),2)
-            counter1= counter1 + sum(np.array(predictions)[:,1]>0.5)
-            counter2= counter2 + len(predictions)- sum(np.array(predictions)[:,1]>0.5)
-            resultMask[int(i/stepsize): int((i + bufferVal)/stepsize) , int(j/stepsize): int((j+ bufferVal)/stepsize),0]=255*qwe[:,:,1]
-            resultMask[int(i/stepsize): int((bufferVal+i)/stepsize) , int(j/stepsize):int((bufferVal +j)/stepsize), 1] = 255*qwe[:, :, 0]
-    endtime = time.time()
-    elapsedtime=endtime-starttime
-    print ('elapsed time ' +  str(elapsedtime))
-    outputname= 'output/' + imgname +'-f'+ str(counter2)+'-o'+ str(counter1)+'.png'
-    plt.imsave(outputname, resultMask, cmap=plt.cm.gray)
-    outputname2 ='output/' + imgname + '-f' + str(counter2) + '-o' + str(counter1) + '.csv'
-    writeXML(outputname2, outputsVec)
-    return (counter2,counter1)
+            qwe = qwe.reshape(int(h / k), int(w / k), 2)
+            counter1 = counter1 + sum(np.array(predictions)[:,1] > 0.5)
+            counter2 = counter2 + len(predictions) - sum(np.array(predictions)[:,1] > 0.5)
+            merged_mask[int(i/step_size): int((i + buffer) / step_size), int(j / step_size): int((j + buffer) / step_size), 0] = 255 * qwe[:, :, 1]
+            merged_mask[int(i/step_size): int((i + buffer) / step_size), int(j / step_size): int((j + buffer) / step_size), 1] = 255 * qwe[:, :, 0]
 
-def main():
-    params=hyperparameterModel.hyperparameterModel()
-    print  (sys.argv)
-    outputpath = ''
-    outputFile = outputpath + 'ver5'
-    # Model training
+    # Save results.
+    out_name = f'{basename(slide_path)}-f{counter2}-o{counter1}'
+    plt.imsave(join(out, out_name + '.png'), merged_mask, cmap=plt.cm.gray)
 
+    # Export to CSV.
+    with open(out_name + '.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(outputsVec)
+
+    return (counter2, counter1)
+
+@click.command()
+@click.option('--slide', help='Path to slide', required=True, metavar='DIR')
+def main(slide):
+
+    # Load model
     tflearn.init_graph()
+    params = hyperparameterModel.hyperparameterModel()
     g = classificationModel3.createModel(params)
     model = tflearn.DNN(g)
-    model.load(outputFile)
-    files = glob.glob("D:/DATA/testForLopez/*.svs")
-    f = open('output/outputlogs.txt', 'w')
-    for myfile in files:
-        print (myfile)
-        results=analyze(myfile,model)
-        f.write((myfile+' ' +str(results[0])+' '+ str(results[1])+'\n'))
+    model.load('ver5')
 
-    f.close()
+    # Generate results for a slide
+    print("Working on: ", slide)
+    results = analyze(slide, model)
+    print("Results: ", results)
 
 if __name__ == "__main__":
     main()
